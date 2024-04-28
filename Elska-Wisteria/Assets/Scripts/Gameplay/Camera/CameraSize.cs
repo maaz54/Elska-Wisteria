a5059767c72d30cd2ed4d750329dd953dd439488
maaz54 <47806802+maaz54@users.noreply.github.com>
2024-04-28 18:41:33 +0500

    Update CameraSize.cs

--- Elska-Wisteria/Assets/Scripts/Gameplay/Camera/CameraSize.cs
+++ Elska-Wisteria/Assets/Scripts/Gameplay/Camera/CameraSize.cs
@@ -7,20 +7,11 @@ namespace Gameplay
 {
     public class CameraSize : MonoBehaviour
     {
-
-        /// <summary>
-        /// cards in the game
-        /// </summary>
-        [SerializeField] Transform[] cards;
-
         /// <summary>
         /// called when cards are generated in the game.
         /// </summary>
         public void OnCardsGenerate(int noOfRow)
         {
-
-            this.cards = cards;
-
             Camera.main.transform.position = new Vector3(0, 0, -noOfRow * 3);
         }
     }
